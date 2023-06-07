55a812ec39d7ac83693578c75dd45a32508cd5f0
stap780 <panaet80@mail.ru>
2023-06-07 16:56:54 +0300

    first

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.17.3"
+
+server '134.122.65.9', port: 22, roles: [:web, :app, :db], primary: true
+set :application, "spreetest"
+set :repo_url, "git@github.com:stap780/spreetest.git"
+
+set :user, 'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+append :linked_files, "config/master.key", "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "public/uploads"
